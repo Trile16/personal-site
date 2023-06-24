5e658368b51bd7c4a73568a7dc49659051358c4e
Tri <16trile@gmail.com>
2023-06-24 15:57:06 -0500

    added plant plaza frontend

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -12,6 +12,51 @@ const Projects = () => {
         <div className="card">
           <div className="project-header">
             <div className="project-title">
+              <img className="project-image" src={plantplaza} />
+              <h3 className="project-name">Plant Plaza</h3>
+            </div>
+            <div>
+              <a
+                className="project-links"
+                href="https://plantplaza.netlify.app"
+                target="_blank"
+              >
+                Deploy
+              </a>
+              <a
+                className="project-links"
+                href="https://github.com/Trile16/plant-plaza"
+                target="_blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="project-links"
+                href="https://plant-plaza-api.fly.dev"
+                target="_blank"
+              >
+                API Docs
+              </a>
+            </div>
+          </div>
+          <p>
+            Description: Plant Wishlist Website created with React and Vite that
+            uses a self built API made with with Node.js, PostgreSQL, Express,
+            and static HTML and CSS. (see API Docs).
+          </p>
+          <p>
+            Overview: This site was created to show proficiency in fullstack
+            development. I love plants, and I collect them as a hobby, so I
+            decided to build out this site as a passion project. After building
+            out my server and database, I was able create a fully functional
+            frontend wishlist site that is able to allow users to register,
+            login, and add a collection of house plants to their wishlist.
+          </p>
+        </div>
+        <hr />
+        <div className="card">
+          <div className="project-header">
+            <div className="project-title">
               <img className="project-image" src={onepgames} />
               <h3 className="project-name">1P Games</h3>
             </div>
@@ -41,42 +86,6 @@ const Projects = () => {
             JavaScript. As a fan of online flash games growing up, this was a
             project that I enjoyed developing. Being able to create these games
             and play them after is what makes the process so satisfying.
-          </p>
-        </div>
-        <hr />
-        <div className="card">
-          <div className="project-header">
-            <div className="project-title">
-              <img className="project-image" src={plantplaza} />
-              <h3 className="project-name">Plant Plaza API</h3>
-            </div>
-            <div>
-              <a
-                className="project-links"
-                href="https://plant-plaza-api.fly.dev"
-                target="_blank"
-              >
-                Deploy
-              </a>
-              <a
-                className="project-links"
-                href="https://github.com/Trile16/plant-plaza-backend"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
-          <p>
-            Description: API for use to build a plant wishlist website created
-            with Node.js, PostgreSQL, Express, and static HTML and CSS.
-          </p>
-          <p>
-            Overview: This site was created to show proficiency in backend
-            development. I love plants, so I built this project to create a
-            database for a list of house plants that I can store on a wishlist.
-            This documentation can be viewed on the deploy and can used to build
-            out a frontend application.
           </p>
         </div>
       </div>
