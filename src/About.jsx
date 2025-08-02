8d6dc4b6b4db9f95f83631c5d287a12f5c426579
Tri <16trile@gmail.com>
2025-08-01 21:17:47 -0500

    updated tech stack

--- src/About.jsx
+++ src/About.jsx
@@ -17,6 +17,19 @@ import python from "./assets/python.svg";
 import docker from "./assets/docker-icon.svg";
 import mongo from "./assets/mongodb.svg";
 import webpack from "./assets/webpack.svg";
+import rust from "./assets/rust.svg"
+import cplusplus from "./assets/cplusplus.svg"
+import java from "./assets/java.svg"
+import sql from "./assets/sql.svg"
+import tokioaxum from "./assets/tokioaxum.png"
+import mysql from "./assets/mysql.svg"
+import observable from "./assets/observable.png"
+import numpy from "./assets/numpy.svg"
+import pycharm from "./assets/pycharm.svg"
+import vscode from "./assets/vscode.svg"
+import git from "./assets/git.svg"
+import github from "./assets/github-original.svg"
+import aws from "./assets/aws.svg"
 
 const About = ({ navColor }) => {
   useEffect(() => {
@@ -44,23 +57,43 @@ const About = ({ navColor }) => {
         <hr />
         <h2 className="about-titles">Tech Stack</h2>
         <hr />
+        <h3 className="about-titles">Languages</h3>
         <div id="tech-container">
-          <TechCard imgSrc={html} name="HTML" />
-          <TechCard imgSrc={css} name="CSS" />
           <TechCard imgSrc={javascript} name="JavaScript" />
           <TechCard imgSrc={typescript} name="TypeScript" />
           <TechCard imgSrc={python} name="Python" />
+          <TechCard imgSrc={cplusplus} name="C++" />
+          <TechCard imgSrc={java} name="Java" />
+          <TechCard imgSrc={rust} name="Rust" />
+          <TechCard imgSrc={html} name="HTML" />
+          <TechCard imgSrc={css} name="CSS" />
+          <TechCard imgSrc={sql} name="SQL" />
+        </div>
+        <h3 className="about-titles">Technologies & Frameworks</h3>
+        <div id="tech-container">
           <TechCard imgSrc={react} name="React" />
           <TechCard imgSrc={nextjs} name="Next.js" />
-          <TechCard imgSrc={mongo} name="MongoDB" />
-          <TechCard imgSrc={postgresql} name="PostgreSQL" />
-          <TechCard imgSrc={express} name="Express" />
           <TechCard imgSrc={nodejs} name="Node.js" />
+          <TechCard imgSrc={express} name="Express" />
+          <TechCard imgSrc={tokioaxum} name="Axum/Tokio" />
           <TechCard imgSrc={redux} name="Redux" />
           <TechCard imgSrc={tailwindcss} name="Tailwind CSS" />
-          <TechCard imgSrc={docker} name="Docker" />
+          <TechCard imgSrc={postgresql} name="PostgreSQL" />
+          <TechCard imgSrc={mysql} name="MySQL" />
+          <TechCard imgSrc={mongo} name="MongoDB" />
+          <TechCard imgSrc={numpy} name="NumPy" />
+          <TechCard imgSrc={observable} name="Observable Plot" />
           <TechCard imgSrc={vitejs} name="Vite" />
           <TechCard imgSrc={webpack} name="Webpack" />
+        </div>
+        <h3 className="about-titles">Tools & Platforms</h3>
+        <div id="tech-container">
+          <TechCard imgSrc={pycharm} name="PyCharm" />
+          <TechCard imgSrc={vscode} name="Visual Studio Code" />
+          <TechCard imgSrc={git} name="Git" />
+          <TechCard imgSrc={github} name="GitHub" />
+          <TechCard imgSrc={aws} name="Amazon Web Services" />
+          <TechCard imgSrc={docker} name="Docker" />
         </div>
         <hr />
       </div>
